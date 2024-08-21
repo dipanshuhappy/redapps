3d50573fbc460a9c40deb6109dc48d08acfc30b6
dipanshuhappy <dipanshuhappy@gmail.com>
2024-08-21 16:35:21 +0530

    remove webiste

--- opshien/contract.py
+++ opshien/contract.py
@@ -1,11 +1,11 @@
 from opshin.prelude import *
+
 @dataclass()
 class WithdrawDatum(PlutusData):
     pubkeyhash: bytes
 
 
 def validator(datum: WithdrawDatum, redeemer: None, context: ScriptContext) -> None:
-    context.tx_info.mint
     sig_present = False
     for s in context.tx_info.signatories:
         if datum.pubkeyhash == s:
